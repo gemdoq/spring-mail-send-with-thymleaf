19e210cddd976abea35980ce94a2dc5787fd01ac
SeungHo <gemdoq@gmail.com>
2024-12-03 16:04:10 +0900

    Initial commit

---
+++ src/test/java/com/example/springmailsendwiththymleaf/SpringMailSendWithThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmailsendwiththymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMailSendWithThymleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
